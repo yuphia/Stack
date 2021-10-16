460f5ac458408a7f54d36857709fdf5af0d70030
yuphia <grentubedark@gmail.com>
2021-10-16 11:47:45 +0300

    Initial commit of my own stack data structure

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include "stackFunctions.h"
+
+int main ()
+{
+    struct stk testStack = {};
+
+    int poppedVal = -100;
+    
+    ctorStk (&testStack, 1, 32);
+
+    $
+
+    printStk (&testStack);
+
+    pushStk (&testStack, 1);
+    printStk (&testStack);
+
+    pushStk (&testStack, 28);
+    printStk (&testStack);
+
+    pushStk (&testStack, 28);
+    printStk (&testStack);
+
+    printf ("ts nelement = %zu\n", testStack.nElement);
+
+    popStk (&testStack, &poppedVal);
+    printStk (&testStack);
+    printf ("popped value = %d\n", poppedVal);
+
+    pushStk (&testStack, 15);
+    printStk (&testStack);
+    
+    popStk (&testStack, &poppedVal);
+    printStk (&testStack);
+    printf ("popped value = %d\n", poppedVal);
+
+    dtorStk (&testStack);
+
+    return NOERR;
+}
